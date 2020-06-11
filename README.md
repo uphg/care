a562ec600fce8d0b5036afaa1154dca1ae0de582
ChenNing02 <2633565885@qq.com>
2020-06-11 17:27:43 +0800

    添加熟悉检查器validator

--- README.md
+++ README.md
@@ -2,8 +2,23 @@
 
 ## 安装
 
-使用本框架前，请开启border-box
+使用本框架前，请开启 border-box
 
 ```html
 box-sizing: border-box;
 ```
+
+## 运行
+
+```sh
+npx parcel index.html
+# 或
+./node_modules/.bin/parcel index.html
+```
+
+如果报错运行：
+
+```sh
+npx parcel index.html --no-cache
+```
+
--- src/button.vue
+++ src/button.vue
@@ -1,8 +1,27 @@
 <template>
-    <button class="c-button">按钮</button>
+    <button class="c-button" :class="{[`icon-${iconPosition}`]: true}">
+        <svg v-if="icon" class="icon" aria-hidden="true">
+            <use :xlink:href="`#i-${icon}`"></use>
+        </svg>
+        <span class="content">
+            <slot></slot>
+        </span>
+    </button>
 </template>
 <script>
-    export default {}
+    export default {
+        props: {
+            icon: {},
+            iconPosition: {
+                type: String, // 类型
+                default: 'left', // 默认值
+                // 属性的检查器
+                validator(value){
+                    return value === 'left' || value === 'right'
+                }
+            }
+        }
+    }
 </script>
 <style lang="scss">
     .c-button {
@@ -12,6 +31,10 @@
         border-radius: var(--border-radius);
         border: 1px solid var(--border-color);
         background: var(--button-bg);
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        vertical-align: middle;
         &:hover {
             border-color: var(--border-color-hover);
         }
@@ -20,6 +43,12 @@
         }
         &:focus {
             outline: none;
+        }
+        > .icon { order: 1; margin-right: .3em; }
+        > .content { order: 2; }
+        &.icon-right {
+            > .icon { order: 2; margin-right: 0; margin-left: .3em; }
+            > .content { order: 1; }
         }
     }
 </style>
