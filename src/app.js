25983b8432d47714fa810e7012ef190f0d2bcfe1
ChenNing02 <2633565885@qq.com>
2020-06-11 20:39:49 +0800

    添加button组功能

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
+import ButtonGroup from './button-group'
 
 Vue.component('c-button', Button)
 Vue.component('c-icon', Icon)
+Vue.component('c-button-group', ButtonGroup)
 
 new Vue({
     el: '#app',
