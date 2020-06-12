d33ecfca7f83f28f2d584514e0f773fb3bc5ac13
ChenNing02 <2633565885@qq.com>
2020-06-12 18:15:09 +0800

    添加几个简单的单元测试

--- src/app.js
+++ src/app.js
@@ -15,3 +15,154 @@ new Vue({
         loading3: false
     }
 })
+
+
+/* 引入chai */
+import chai from 'chai'
+const expect = chai.expect
+
+/* 单元测试 */
+{
+    // 吧Button组件变为构造函数
+    const Constructor = Vue.extend(Button)
+    // new 一个构造函数的实例
+    const button = new Constructor({
+        // 给组件传入一个icon属性
+        propsData: {
+            icon: 'settings'
+        }
+    })
+    // 将实例添加到内存中
+    button.$mount()
+    // 获取 button 中的 svg 图标 use
+    let useElement = button.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    // 断言它的 xlink:href 为 #i-settings
+    expect(href).to.eq('#i-settings')
+    // 每次断言完成后删除button元素
+    button.$el.remove()
+    // 删除button对象
+    button.$destroy()
+}
+/* 测试 loading，如果 loading 为 true 的状态下 use 的 xlink:href 应该为"#i-loading" */
+{
+    // 吧Button组件变为构造函数
+    const Constructor = Vue.extend(Button)
+    // new 一个构造函数的实例
+    const button = new Constructor({
+        // 给组件传入一个icon属性
+        propsData: {
+            icon: 'settings',
+            loading: true
+        }
+    })
+    // 将实例添加到内存中
+    button.$mount()
+    // 获取 button 中的 svg 图标 use
+    let useElement = button.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    // 断言它的 xlink:href 为 #i-loading
+    expect(href).to.eq('#i-loading')
+    // 每次断言完成后删除button元素
+    button.$el.remove()
+    // 删除button对象
+    button.$destroy()
+}
+
+/* 测试 Button按钮的 css - order属性（flex的子元素的order属性）默认属性为 order: 1，断言它的值是否为 1 */
+{
+    // 创建一个div并将它塞到body中，用于存放button，因为button只有出现在页面中才会渲染css属性
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    // 吧Button组件变为构造函数
+    const Constructor = Vue.extend(Button)
+    // new 一个构造函数的实例
+    const button = new Constructor({
+        // 给组件传入一个icon属性
+        propsData: {
+            icon: 'settings'
+        }
+    })
+    // 将实例添加到上面创建的div中
+    button.$mount(div)
+    let svg = button.$el.querySelector('svg')
+    // 获取svg的CSS属性order，的值
+    let order = window.getComputedStyle(svg).order
+    // 此处的1应该是字符串，所有属性获取后都为字符串
+    expect(order).to.eq('1')
+    // 每次断言完成后删除button元素
+    button.$el.remove()
+    // 删除button对象
+    button.$destroy()
+}
+
+/* 测试 Button按钮的图标位置，就是图标的 css - order属性（flex的子元素的order属性）默认属性为 order: 1，断言它的值是否为 1 */
+{
+    // 创建一个div并将它塞到body中，用于存放button，因为button只有出现在页面中才会渲染css属性
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    // 吧Button组件变为构造函数
+    const Constructor = Vue.extend(Button)
+    // new 一个构造函数的实例
+    const button = new Constructor({
+        // 给组件传入一个icon属性
+        propsData: {
+            icon: 'settings'
+        }
+    })
+    // 将实例添加到上面创建的div中
+    button.$mount(div)
+    let svg = button.$el.querySelector('svg')
+    // 获取svg的CSS属性order，的值
+    let order = window.getComputedStyle(svg).order
+    // 此处的1应该是字符串，所有属性获取后都为字符串
+    expect(order).to.eq('1')
+    // 每次断言完成后删除button元素
+    button.$el.remove()
+    // 删除button对象
+    button.$destroy()
+}
+
+/* 测试 Button按钮图标位置在最后时 css - order属性（flex的子元素的order属性）默认属性为 order: 1，断言它的值是否为 2，也就是是否靠后 */
+{
+    // 创建一个div并将它塞到body中，用于存放button，因为button只有出现在页面中才会渲染css属性
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    // 吧Button组件变为构造函数
+    const Constructor = Vue.extend(Button)
+    // new 一个构造函数的实例
+    const button = new Constructor({
+        // 给组件传入一个icon属性
+        propsData: {
+            icon: 'settings',
+            iconPosition: 'right'
+        }
+    })
+    // 将实例添加到上面创建的div中
+    button.$mount(div)
+    let svg = button.$el.querySelector('svg')
+    // 获取svg的CSS属性order，的值
+    let order = window.getComputedStyle(svg).order
+    // 此处的值应该为2，因为它是放在最后的图标
+    expect(order).to.eq('2')
+     // 每次断言完成后删除button元素
+     button.$el.remove()
+     // 删除button对象
+     button.$destroy()
+}
+
+/* 测试button的click事件有无问题 */
+{
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'settings'
+        }
+    })
+    vm.$mount()
+    vm.$on('click', function(){
+        console.log(111)
+    })
+    let button = vm.$el
+    button.click()
+}
